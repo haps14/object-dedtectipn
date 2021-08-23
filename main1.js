e1afe8bf103a973c86cfe65bc489cef0745d90ec
haps14 <79513622+haps14@users.noreply.github.com>
2021-08-23 21:07:34 +0530

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,29 @@
+wall = ""
+
+function n(){
+
+        wall = "bed_and_other_things";
+        window.location = "index.html";
+}
+
+function r(){
+
+    wall = "tv_and_other_things";
+    window.location = "index.html";
+}
+
+function s(){
+
+      wall = "laptop_and_phone";
+      window.location = "index.html";
+}
+
+function v(){
+    wall = "fruit_basket";
+    window.location = "index.html";
+}
+
+function z(){
+    wall = "types_of_bottle";
+    window.location = "index.html";
+}
